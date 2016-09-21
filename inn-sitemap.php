244cbb9107478a61b9b81d18c3b500c45fba3c0e
Roy van der Loo <info@arrays.nl>
2016-09-21 13:19:58 +0200

    Sitemap v1

---
+++ inn-sitemap.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Plugin Name: Internative sitemap
+Plugin URI: www.internative.nl
+Description: Sitemap shortcode functionality.
+Author: Internative
+Version: 1.0.0
+Author URI: www.internative.nl
+License: GPLv2 or later
+Text Domain: inn-sitemap
+Domain Path: /languages
+GitHub Plugin URI: InternativeNL/inn-sitemap
+*/
+
+///////////////////////////////////////////////////////////////////////////////
+
+require (dirname(__FILE__).'/inc_shortcodes.php');
+
+///////////////////////////////////////////////////////////////////////////////
